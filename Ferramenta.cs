aa47eac87740a89da0ec43625f7a65f0223a98ef
Samuel Bacellar <samuelbacellar1@gmail.com>
2022-04-09 16:27:28 -0300

    Criando código do projeto
    Projeto programado em C# (orientação a objetos) utilizando os Destruidores da programação orientada a objetos.

---
+++ Ferramenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestruidores
+{
+    public class Ferramenta
+    {
+        public Ferramenta(String nome)
+        {
+            this.Nome = nome;
+        }
+
+        ~Ferramenta()
+        {
+            Console.WriteLine("A sua ferramenta " + this.Nome + " foi destruída");
+        }
+
+        public String Nome { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestruidores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ferramenta f1 = new Ferramenta("Martelo");
+            Ferramenta f2 = new Ferramenta("Martelete");
+            f1 = null;
+            f2 = new Ferramenta("Marreta");
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
